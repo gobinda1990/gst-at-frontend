8266ea7495c5c30fced0234e326612607a3f3c0b
gobinda1990 <68889122+gobinda1990@users.noreply.github.com>
2026-09-07 15:49:14 +0530

    Update apiClient.js

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const dashboardClient =
   axios.create({
 
-    baseURL:"http://localhost:8085/api",
+    baseURL:"http://10.153.43.8:8087/api",
    // timeout: 30000,
     headers: {
       "Content-Type":
