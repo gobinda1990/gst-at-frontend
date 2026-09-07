d05ac3894bcb1def210162ae1f647999c207ff81
gobinda1990 <68889122+gobinda1990@users.noreply.github.com>
2026-09-07 17:03:13 +0530

    Update apiClient.js

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const dashboardClient =
   axios.create({
 
-    baseURL:"/api",
+    baseURL:"/api/gst/return-3b",
    // timeout: 30000,
     headers: {
       "Content-Type":
