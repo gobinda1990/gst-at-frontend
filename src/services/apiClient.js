d295c4e717703390bf7a986b6d78b6aac27261f1
gobinda <sasmal.gobind04@gmail.com>
2026-09-08 15:18:33 +0530

    okk
    Signed-off-by: gobinda <sasmal.gobind04@gmail.com>

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -3,11 +3,8 @@ import axios from "axios";
 export const dashboardClient =
   axios.create({
 
-<<<<<<< HEAD
-    baseURL:"http://localhost:8087/api",
-=======
+    // baseURL:"http://localhost:8087/api",
     baseURL:"/api",
->>>>>>> 47fd8305749638f6762be7f3301ba346fad7fbf0
    // timeout: 30000,
     headers: {
       "Content-Type":
